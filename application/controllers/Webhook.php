1b7ea1f722ab53510c3527bcc8f2043cf53af84c
github-andi <32433484+github-andi@users.noreply.github.com>
2017-10-04 16:59:56 +0700

    Update Webhook.php

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -138,7 +138,7 @@ class Webhook extends CI_Controller {
     $this->bot->replyMessage($event['replyToken'], $multiMessageBuilder);
   }
   
-  private function tambaahTugas($replyToken, $user)
+  private function tambahTugas($replyToken, $user)
   {
    $message = "Masukkan nama tugas :\n";
    $textMessageBuilder = new TextMessageBuilder($message);
